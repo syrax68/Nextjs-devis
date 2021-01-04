319a7435cca36b952b58d27349f0bbefd131766f
syrax <syraxrakotonarivo@gmail.com>
2021-01-04 08:58:40 +0300

    modification countries

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Devis from './components/DevisModal'
-import {
-  Button
-} from '@material-ui/core';
 
 export default function Home() {
   return (
